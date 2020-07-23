6abd24fe27078b09bac646bf13bc9d1e5c3f4369
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2020-07-23 10:24:15 +0200

    Add column widths docs

--- .vuepress/3.1.js
+++ .vuepress/3.1.js
@@ -35,7 +35,6 @@ module.exports = [
             'multiple-sheets',
             'mapping',
             'column-formatting',
-            'custom-formatting-values',
             'settings',
             'drawings',
             'concerns',
