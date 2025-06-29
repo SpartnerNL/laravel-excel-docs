53444dabe0ff658e574ca9b532e15c1e4dd3b3eb
Kevin Bui <hoanglongbui89@yahoo.com.vn>
2025-06-29 14:05:10 +1000

    Add a nav item named headings to the side bar.

--- .vuepress/3.1.js
+++ .vuepress/3.1.js
@@ -35,6 +35,7 @@ module.exports = [
             'queued',
             'multiple-sheets',
             'mapping',
+            'headings',
             'column-formatting',
             'settings',
             'drawings',
